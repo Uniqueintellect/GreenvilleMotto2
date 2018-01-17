b6bf2c9daf715784b25c530e25d22d843a8806dd
Zachary Johnson <jl7164ut@my.lsc.edu>
2018-01-17 11:28:15 -0800

    added a comment

--- GreenvilleMotto2/Program.cs
+++ GreenvilleMotto2/Program.cs
@@ -1,12 +1,12 @@
 ﻿using static System.Console;
 
 namespace GreenvilleMotto2
-{
+
     class Program
     {
         static void Main(string[] args)
         {
-            WriteLine("**********************************");
+            WriteLine("**********************************");//This creates a border of stars
             WriteLine("* The stars shine in Greenville. *");
             WriteLine("**********************************");
         }
